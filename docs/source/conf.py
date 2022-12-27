7d10308c405c4a880f9c824db18dd4a43481fd2e
Yusuf Manshur <ymanshur@gmail.com>
2022-12-27 07:00:20 +0700

    feat: add sphinxcontrib-contentui module

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,11 @@ author = 'Laura'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", 'sphinxcontrib.httpdomain']
+extensions = [
+    'myst_parser',
+    'sphinxcontrib.httpdomain',
+    'sphinxcontrib.contentui'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,4 +28,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
